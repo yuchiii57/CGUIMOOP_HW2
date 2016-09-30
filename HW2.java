903842d8081bdbadd4b915e3b52912959e32a468
YiJu Tseng <beergt@gmail.com>
2016-09-30 19:31:03 +0800

    UPDATE SAMPLE

--- HW2.java
+++ HW2.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Scanner;
 /*
  * ID: TODO: please add student ID here
  * Name: TODO: please add student name here
@@ -8,8 +8,13 @@ import java.util.HashMap;
 public class HW2 {
 
 	public static void main(String[] args) {
-		int nDeck=Integer.parseInt(args[0]);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input N(deck of cards):");
+		String testn= sc.nextLine(); 
+        
+		int nDeck=Integer.parseInt(testn);
 		Deck deck=new Deck(nDeck);
+		//TODO: please check your output, make sure that you print all cards on your screen
 		deck.printDeck();
 		
 		if(isAllCardsCorrect(deck.getAllCards(),nDeck)){
@@ -19,7 +24,7 @@ public class HW2 {
 		}
 	}
 	/*
-	 * This method is used for checking your result, not a part of your HW
+	 * This method is used for checking your result, not a part of your HW2
 	 */
 	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
 		//check the output 
@@ -28,7 +33,7 @@ public class HW2 {
 		for(Card card:allCards){
 			int suit= card.getSuit();
 			int rank = card.getRank();
-			if(suit>13||suit<1||rank>4||rank<1){
+			if(suit>4||suit<1||rank>13||rank<1){
 				isCorrect=false;
 				break;
 			}
@@ -57,41 +62,43 @@ public class HW2 {
 /*
  * Description: TODO: please add description here
  */
-//TODO: Please implement the class
 class Deck{
 	private ArrayList<Card> cards;
 	//TODO: Please implement the constructor
 	public Deck(int nDeck){
+		cards=new ArrayList<Card>();
 		//1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
-		//Hint: Use new Card(x,y) and for loop to add card into deck
+		//Hint: Use new Card(x,y) and 3 for loops to add card into deck
 		//Sample code start
-		//Card card=new Card(1,1);
+		//Card card=new Card(1,1); ->means new card as clubs ace
 		//cards.add(card);
 		//Sample code end
+
 	}	
 	//TODO: Please implement the method to print all cards on screen
 	public void printDeck(){
-		//Hint: print all items in ArrayList<Card> cards
+		//Hint: print all items in ArrayList<Card> cards, 
+		//please implement and reuse printCard method in Card class
+
 	}
-	//TODO: Please implement the method to return all class in 
 	public ArrayList<Card> getAllCards(){
 		return cards;
 	}
 }
 /*
- * Description: TODO: 
+ * Description: TODO: please add description here
  */
-//TODO: Please implement the class
 class Card{
-	private int suit; //Definition: Clubs=1, Diamonds=2, Hearts=3, Spades=4
+	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
 	private int rank; //1~13
-	//TODO: Please implement the constructor
 	public Card(int s,int r){
 		suit=s;
 		rank=r;
 	}	
+	//TODO: 1. Please implement the printCard method
 	public void printCard(){
-		//Hint: print card as suit,rank, for example: 1,1 as Clubs Ace
+		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
+
 	}
 	public int getSuit(){
 		return suit;
